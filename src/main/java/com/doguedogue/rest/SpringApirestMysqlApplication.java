544f09a77a1733910c4cb16ef76ebe5f411013e3
Rafael Vilches <rvilches38@yahoo.com.mx>
2023-03-25 23:55:07 -0600

    first commit

---
+++ src/main/java/com/doguedogue/rest/SpringApirestMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.doguedogue.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApirestMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApirestMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doguedogue/rest/SpringApirestMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doguedogue.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApirestMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
