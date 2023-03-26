3788a3b02fc9afcd675874a7190aebe3f1c02fe1
Rafael Vilches <rvilches38@yahoo.com.mx>
2023-03-26 00:42:27 -0600

    add db set up, entity and lombok

---
+++ src/main/java/com/doguedogue/rest/model/entity/Cliente.java
@@ -0,0 +1,46 @@
+package com.doguedogue.rest.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Table (name="clientes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Cliente {
+
+	
+	@Id
+	@GeneratedValue (strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "apellido")
+	private String apellido;
+	
+	@Column(name = "email")
+	private String email;
+
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+}
