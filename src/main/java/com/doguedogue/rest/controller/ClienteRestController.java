d2af6cf3dfc12b8826b308315031d3caa5683ad6
Rafael Vilches <rvilches38@yahoo.com.mx>
2023-03-26 01:22:36 -0600

    add Cors

--- src/main/java/com/doguedogue/rest/controller/ClienteRestController.java
+++ src/main/java/com/doguedogue/rest/controller/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.doguedogue.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.doguedogue.rest.model.entity.Cliente;
 import com.doguedogue.rest.model.service.IClienteService;
 
+@CrossOrigin (origins="http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
