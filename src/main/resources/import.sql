05f9dcbdfb0ce4041014a469a276f7bd3306bbb9
Rafael Vilches <rvilches38@yahoo.com.mx>
2023-03-26 01:15:02 -0600

    add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('John', 'Rambo', 'rambo@yahoo.com', '2017-12-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Silvester', 'Stallone', 'stallone@yahoo.com', '2017-05-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Sarah', 'Connor', 'connor@yahoo.com', '2018-04-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Pedro', 'Paramo', 'paramo@yahoo.com', '2010-03-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Pablo', 'Marmol', 'marmolp@yahoo.com', '2022-02-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Martin', 'Garrix', 'garrix@yahoo.com', '2023-01-05');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Jesica', 'Andrade', 'jesica.andrade@yahoo.com', '2011-06-06');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Joaquin', 'Phoenix', 'phoenix@yahoo.com', '2011-07-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Jorge', 'Travies', 'traviesj@yahoo.com', '2001-08-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Enrique', 'Villegas', 'villegaze@yahoo.com', '2002-09-09');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Luis', 'Ramirez', 'ramirez@yahoo.com', '2010-11-10');
