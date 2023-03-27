3dd0d6cfe2e82414239c41a0434f4ccc3df979c8
Rafael Vilches <rvilches38@yahoo.com.mx>
2023-03-26 23:21:00 -0600

    add validations and exceptions

--- src/main/java/com/doguedogue/rest/controller/ClienteRestController.java
+++ src/main/java/com/doguedogue/rest/controller/ClienteRestController.java
@@ -1,10 +1,12 @@
 package com.doguedogue.rest.controller;
 
-//import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,14 +35,30 @@ public class ClienteRestController {
 	}
 	
 	@GetMapping ("/clientes/{id}")
-	public Cliente show(@PathVariable Long id){
-		return clienteService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id){
+		
+		Cliente cliente = null;
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			cliente = clienteService.findById(id);
+		}catch(Exception e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getCause().toString()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if (cliente == null) {
+			response.put("mensaje", "El cliente ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
 	}
 	
 	@PostMapping ("/clientes")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente create(@RequestBody Cliente cliente) {
-//		cliente.setCreatedAt(new Date());
 		return clienteService.save(cliente);
 	}
 	
